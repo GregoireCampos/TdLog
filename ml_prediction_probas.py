6210806618daebb866ebdabeab1af59be972354c
Badr <badr@Badrs-MacBook.local>
2019-12-30 20:03:17 +0100

    algo de ml avec proba

--- ml_prediction_probas.py
+++ ml_prediction_probas.py
@@ -22,8 +22,8 @@ from sklearn import datasets
 from sklearn.metrics import confusion_matrix 
 from sklearn.model_selection import train_test_split 
 
-df =pd.read_csv('training_file.csv', sep=';')
-
+df =pd.read_csv('F1_processed.csv', sep=',')
+#sep = "," fot F1_processed or sep = ";" for training_file
 df_dict = df.to_dict()
 keys_to_keep = ["FTR","B365H","B365D","B365A","HTGDBG","ATGDBG","HTPBG","ATPBG"]
 #I only keep "before-game data" except FTR which I will use to train my classification algorithm
@@ -51,28 +51,56 @@ testing_Y = Y[int(len(Y)-len(Y)/5):]
 
 from sklearn.tree import DecisionTreeClassifier 
 dtree_model = DecisionTreeClassifier(max_depth = 10).fit(training_X, training_Y) 
-dtree_predictions = dtree_model.predict(testing_X) 
-  
-# creating a confusion matrix 
-#cm = confusion_matrix(testing_Y, dtree_predictions) 
-#true_class = cm[0][0]+cm[1][1]+cm[2][2]
-#print("Decision tree correct answers (%): ")
-#print(true_class/len(testing_Y))
+dtree_predictions = dtree_model.predict_proba(testing_X) 
+
+threshold = 0.5
+accepted_games = []
+accepted_odd =[]
+accepted_Y = []
+for k in range (len(dtree_predictions)) : 
+    for i in range(len(dtree_predictions[k])) : 
+        if dtree_predictions[k][i]>threshold : 
+            accepted_games+=[i]
+            accepted_Y+=[testing_Y[k]]
+            accepted_odd+=[testing_X[k][i]]
+#creating a confusion matrix 
+cm = confusion_matrix(accepted_Y, accepted_games) 
+true_class = cm[0][0]+cm[1][1]+cm[2][2]
+print("Decision tree correct answers (%): ")
+print(true_class/len(accepted_Y))
+print("cote moyenne")
+print(sum(accepted_odd)/len(accepted_odd))
+print("Gain moyen par match pour 20E par mise  : ")
+print((true_class/len(accepted_Y))*(sum(accepted_odd)/len(accepted_odd))*20-20)
+print("-----------------")
 
 
 # training a linear SVM classifier 
-#from sklearn.svm import SVC 
-#svm_model_linear = SVC(kernel = 'linear', C = 1, probability=True).fit(training_X, training_Y) 
-#svm_predictions = svm_model_linear.predict(testing_X) 
-  
-# model accuracy for X_test   
-#accuracy = svm_model_linear.score(testing_X, testing_Y) 
-  
-## creating a confusion matrix 
-#cm = confusion_matrix(testing_Y, svm_predictions)
-#true_class = cm[0][0]+cm[1][1]+cm[2][2]
-#print(" SVM correct answers (%): ")
-#print(true_class/len(testing_Y)) 
+from sklearn.svm import SVC 
+svm_model_linear = SVC(kernel = 'linear', C = 1, probability = True).fit(training_X, training_Y) 
+svm_predictions = svm_model_linear.predict_proba(testing_X) 
+
+accepted_games = []
+accepted_Y = []
+for k in range (len(svm_predictions)) : 
+    for i in range(len(svm_predictions[k])) : 
+        if svm_predictions[k][i]>threshold : 
+            accepted_games+=[i]
+            accepted_Y+=[testing_Y[k]]
+            accepted_odd+=[testing_X[k][i]]
+#model accuracy for X_test   
+accuracy = svm_model_linear.score(testing_X, testing_Y) 
+
+# creating a confusion matrix 
+cm = confusion_matrix(accepted_Y, accepted_games)
+true_class = cm[0][0]+cm[1][1]+cm[2][2]
+print(" SVM correct answers (%): ")
+print(true_class/len(accepted_Y))
+print("cote moyenne")
+print(sum(accepted_odd)/len(accepted_odd))
+print("Gain moyenpar match pour 20E par mise  : ")
+print((true_class/len(accepted_Y))*(sum(accepted_odd)/len(accepted_odd))*20-20)
+print("-----------------")
 
 # training a KNN classifier 
 #from sklearn.neighbors import KNeighborsClassifier 
